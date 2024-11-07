9368c0384460aeee932a85488f71076131047887
Mark Abig <mark.hanherly@gmail.com>
2024-11-07 18:37:56 +0400

    Exercise 06 - Brute Force Attack

---
+++ Assessments/Brute Force.py
@@ -0,0 +1,23 @@
+tries = 5
+while tries > 0:
+    password = input('whats the correct password? ')
+    correctpw = "Skibidi69"
+    correctpw != password
+    if correctpw == password:
+        print('thats the correct password!')
+        break
+    else:
+        print('thats the wrong password try again!')
+        tries -= 1
+        if tries == 0:
+            print('Max tries reached authorities have been alerted')
+            break
+        print(str(tries) + ' tries left')
+
+
+
+
+
+ 
+
+
