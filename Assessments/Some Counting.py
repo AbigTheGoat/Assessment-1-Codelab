000d2e21936ed2463a6efa9fac70f82ba44de5fd
Mark Abig <mark.hanherly@gmail.com>
2024-11-09 22:21:57 +0400

    Create Some Counting.py

---
+++ Assessments/Some Counting.py
@@ -0,0 +1,16 @@
+
+#0 to 50
+for x in range (0,51,1):
+    print(x)
+#50 to 0
+for x in range (50,-1,-1):
+    print(x)
+#30 to 50
+for x in range (30,51,1):
+    print(x)
+#50 to 10
+for x in range (50,9,-2):
+    print(x)
+#100 to 200
+for x in range (100,205,5):
+    print(x)
