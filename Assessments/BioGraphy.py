1ff18af118726638c62dd4941d6d990005785f36
Mark Hanherly Abig <mark.hanherly@gmail.com>
2024-11-10 00:20:06 +0400

    Added commentspy

--- Assessments/BioGraphy.py
+++ Assessments/BioGraphy.py
@@ -1,11 +1,11 @@
-
-Dictionar = {
+#Mark Hanherly Abig Cybersec year 1
+Dictionar = { #Dictionary is composed of Keys and values that are able to be called out
 "Name": str(input('what is your name?' )),
     
     "Age": str(input('How old are you?' )),
 
     "Hometown": str(input('wheres your hometown?' ))
-}
+} 
 
 for key, value in Dictionar.items():
     print(f"{key}, {value}")
